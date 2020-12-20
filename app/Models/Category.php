0f105d9eccbb385c720f0edee127c2d6d31f9c71
Jeam Alves <jeanmalves@gmail.com>
2020-12-20 04:23:51 +0000

    Added cast to id because use uuid and use trait

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use App\Models\Traits\Uuid;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, Uuid;
     protected $fillable = ['name', 'description', 'is_active'];
     protected $dates = ['deleted_at'];
+    protected $casts = ['id' => 'string'];
 }
