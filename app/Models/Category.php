d8bbd777424ff41db295dd55a8c38f58de4cb003
Jeam Alves <jeanmalves@gmail.com>
2020-12-30 02:53:13 +0000

    just space :lipstick:

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Category extends Model
 {
     use SoftDeletes, Uuid;
+    
     protected $fillable = ['name', 'description', 'is_active'];
     protected $dates = ['deleted_at'];
     protected $casts = ['id' => 'string'];
