81e462df9ff8deffd53201a17362ed8f205cd2cb
Jeam Alves <jeanmalves@gmail.com>
2020-12-30 03:27:44 +0000

    Route genre resource

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,12 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::group(['namespace' => 'Api'], function () {
-    Route::resource('categories', 'CategoryController', ['except' => ['create', 'edit']]);
+    $exceptCreateAndEdit = [
+        'except' => ['create', 'edit']
+    ];
+    
+    Route::resource('categories', 'CategoryController', $exceptCreateAndEdit);
+    Route::resource('genres', 'GenreController', $exceptCreateAndEdit);
 });
 
 
