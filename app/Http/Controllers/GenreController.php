72b36b0752f2ebd18a00ed6b16548c4a6aac4728
Jeam Alves <jeanmalves@gmail.com>
2020-12-30 03:08:22 +0000

    Added genre resource controller

--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -7,79 +7,35 @@ use Illuminate\Http\Request;
 
 class GenreController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    private $rules = [
+        'name' => 'required|max:255',
+        'is_active' => 'boolean' 
+    ];
+
     public function index()
     {
-        //
+        return Genre::all();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, $this->rules);
+
+        return Genre::create($request->all());
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Genre  $genre
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Genre $genre)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Genre  $genre
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Genre $genre)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Genre  $genre
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Genre $genre)
     {
-        //
+        $this->validate($request, $this->rules);
+        $genre->update($request->all());
+
+        return $genre;
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Genre  $genre
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Genre $genre)
     {
-        //
+        $genre->delete();
+
+        return response()->noContent();
     }
 }
