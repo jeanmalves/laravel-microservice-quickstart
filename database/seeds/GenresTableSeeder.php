bf7b365e9c3d428eb99d0aa849c20272e765de96
Jeam Alves <jeanmalves@gmail.com>
2020-12-30 03:07:25 +0000

    Fixes :lipstick:

--- database/seeds/GenresTableSeeder.php
+++ database/seeds/GenresTableSeeder.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\Genre;
 use Illuminate\Database\Seeder;
 
 class GenresTableSeeder extends Seeder
@@ -12,6 +11,6 @@ class GenresTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(Genre::class, 30)->create();
+        factory(App\Models\Genre::class, 100)->create();
     }
 }
