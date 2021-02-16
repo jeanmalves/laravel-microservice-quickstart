d06dd23748bb7d84291109b4b1b1de7757771406
Jeam Alves <jeanmalves@gmail.com>
2021-02-16 01:56:35 +0000

    Attribute boolean cast

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -12,7 +12,10 @@ class Genre extends Model
 
     protected $fillable = ['name', 'is_active'];
     protected $dates = ['deleted_at'];
-    protected $casts = ['id' => 'string'];
+    protected $casts = [
+        'id' => 'string',
+        'is_active' => 'boolean'
+    ];
 
     public $incrementing = false;
 }
